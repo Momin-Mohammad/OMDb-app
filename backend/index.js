52737d9e684f2ad100212fa8aea7aa02e35577e6
Mohammad Momin <mominmohammad111@gmail.com>
2023-11-06 23:35:04 +0530

    resolved sors issue

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors({origin:'*'}));
+app.use(cors());
 app.use("/movie",movieRouter);
 
 app.get("/",(req,res)=>{
