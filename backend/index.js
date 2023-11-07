0e1119cce840fe0737e31489f1a28c0df32a65c4
Mohammad Momin <mominmohammad111@gmail.com>
2023-11-07 18:38:16 +0530

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use("/movie",movieRouter);
 
 app.get("/",(req,res)=>{
